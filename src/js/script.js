5daa2f2069d6b9d65619b69914baf9617d3126e3
Adriano Jr <adrianojunr@gmail.com>
2025-03-03 00:56:23 -0300

    Add limitação de tempo máximo

--- src/js/script.js
+++ src/js/script.js
@@ -20,141 +20,160 @@ let modo_pausa = false;
 let timer_interval;
 
 const formatTime = (minutes, seconds) => {
-    return `
+  return `
     ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 };
 
 const toggleConfigPanel = () => {
-    clickSound()
-    config_container.style.display =
-        config_container.style.display === "grid" ? "none" : "grid";
-    container.style.display =
-        config_container.style.display === "grid" ? "none" : "flex";
+  clickSound();
+  config_container.style.display =
+    config_container.style.display === "grid" ? "none" : "grid";
+  container.style.display =
+    config_container.style.display === "grid" ? "none" : "flex";
 };
 
 const validateAndSaveConfig = () => {
-    minutos_trabalho = parseInt(input_minutos_trabalho.value);
-    minutos_pausa = parseInt(input_minutos_pausa.value);
+  minutos_trabalho = parseInt(input_minutos_trabalho.value);
+  minutos_pausa = parseInt(input_minutos_pausa.value);
 
-    const error_minutos = document.getElementById("error-minutos");
-    const error_minutos_pausa = document.getElementById("error-minutos-pausa");
-    let has_error = false;
+  const error_minutos = document.getElementById("error-minutos");
+  const error_minutos_pausa = document.getElementById("error-minutos-pausa");
+  let has_error = false;
 
-    resetErrorsConfig();
+  resetErrorsConfig();
 
-    if (isNaN(minutos_trabalho) || minutos_trabalho < 15) {
-        displayError(
-            error_minutos,
-            "Mínimo de 15 minutos.",
-            input_minutos_trabalho,
-            "error-input"
-        );
-        has_error = true;
-    }
-    if (isNaN(minutos_pausa) || minutos_pausa < 5) {
-        displayError(
-            error_minutos_pausa,
-            "Mínimo de 5 minutos.",
-            input_minutos_pausa,
-            "error-input"
-        );
-        has_error = true;
-    }
+  if (isNaN(minutos_trabalho) || minutos_trabalho < 15) {
+    displayError(
+      error_minutos,
+      "Mínimo de 15 minutos.",
+      input_minutos_trabalho,
+      "error-input",
+    );
+    has_error = true;
+  }
+  if (isNaN(minutos_pausa) || minutos_pausa < 5) {
+    displayError(
+      error_minutos_pausa,
+      "Mínimo de 5 minutos.",
+      input_minutos_pausa,
+      "error-input",
+    );
+    has_error = true;
+  }
 
-    if (has_error) return;
+  if (isNaN(minutos_trabalho) || minutos_trabalho > 60) {
+    displayError(
+      error_minutos,
+      "Não é recomendado mais de 60 minutos de foco.",
+      input_minutos_trabalho,
+      "error-input",
+    );
+    has_error = true;
+  }
+  if (isNaN(minutos_pausa) || minutos_pausa > 60) {
+    displayError(
+      error_minutos_pausa,
+      "Não é recomendado mais de 60 minutos de pausa",
+      input_minutos_pausa,
+      "error-input",
+    );
+    has_error = true;
+  }
 
-    tempo_restante = minutos_trabalho * 60;
-    clock_timer.textContent = formatTime(minutos_trabalho, 0);
+  if (has_error) return;
 
-    toggleConfigPanel();
+  tempo_restante = minutos_trabalho * 60;
+  clock_timer.textContent = formatTime(minutos_trabalho, 0);
+
+  toggleConfigPanel();
 };
 
 const displayError = (element, message, input, name) => {
-    element.textContent = message;
-    element.style.display = "block";
-    input.classList.add(name);
+  element.textContent = message;
+  element.style.display = "block";
+  input.classList.add(name);
 };
 
 const resetErrorsConfig = () => {
-    const error_minutos = document.getElementById("error-minutos");
-    const error_minutos_pausa = document.getElementById("error-minutos-pausa");
+  const error_minutos = document.getElementById("error-minutos");
+  const error_minutos_pausa = document.getElementById("error-minutos-pausa");
 
-    error_minutos.textContent = "";
-    error_minutos.style.display = "none";
+  error_minutos.textContent = "";
+  error_minutos.style.display = "none";
 
-    error_minutos_pausa.textContent = "";
-    error_minutos_pausa.style.display = "none";
+  error_minutos_pausa.textContent = "";
+  error_minutos_pausa.style.display = "none";
 
-    input_minutos_trabalho.classList.remove("error-input");
-    input_minutos_pausa.classList.remove("error-input");
+  input_minutos_trabalho.classList.remove("error-input");
+  input_minutos_pausa.classList.remove("error-input");
 };
 
 const startTimer = () => {
-    if (timer_ativo) return;
-    
-    timer_ativo = true;
-    timer_interval = setInterval(updateTimer, 1000);
-    clickSound()
+  if (timer_ativo) return;
+
+  timer_ativo = true;
+  timer_interval = setInterval(updateTimer, 1000);
+  clickSound();
 };
 
 const updateTimer = () => {
-    if (tempo_restante <= 0) {
-        handleTimerEnd();
-        return;
-    }
+  if (tempo_restante <= 0) {
+    handleTimerEnd();
+    return;
+  }
 
-    tempo_restante--;
-    const minutos = Math.floor(tempo_restante / 60);
-    const segundos = tempo_restante % 60;
-    clock_timer.textContent = formatTime(minutos, segundos);
+  tempo_restante--;
+  const minutos = Math.floor(tempo_restante / 60);
+  const segundos = tempo_restante % 60;
+  clock_timer.textContent = formatTime(minutos, segundos);
 };
 
 const handleTimerEnd = () => {
-    if (!modo_pausa) {
-        clearInterval(timer_interval);
-        timer_ativo = false;
-        modo_pausa = true;
-        tempo_restante = minutos_pausa * 60;
-        clock_timer.textContent = formatTime(minutos_pausa, 0);
-        startTimer();
-    } else {
-        clearInterval(timer_interval);
-        timer_ativo = false;
-        modo_pausa = false;
-    }
+  if (!modo_pausa) {
+    clearInterval(timer_interval);
+    timer_ativo = false;
+    modo_pausa = true;
+    tempo_restante = minutos_pausa * 60;
+    clock_timer.textContent = formatTime(minutos_pausa, 0);
+    startTimer();
+  } else {
+    clearInterval(timer_interval);
+    timer_ativo = false;
+    modo_pausa = false;
+  }
 };
 
 const pauseTimer = () => {
-    clearInterval(timer_interval);
-    timer_ativo = false;
-    clickSound()
+  clearInterval(timer_interval);
+  timer_ativo = false;
+  clickSound();
 };
 
 button_config.addEventListener("click", toggleConfigPanel);
 button_save.addEventListener("click", validateAndSaveConfig);
 btn_pause.addEventListener("click", pauseTimer);
 btn_start.addEventListener("click", startTimer);
-btn_radio.addEventListener('click', radio);
-btn_pause_radio.addEventListener('click', pause)
+btn_radio.addEventListener("click", radio);
+btn_pause_radio.addEventListener("click", pause);
 
-function radio(){
-    btn_radio.disabled = true;
-    btn_pause_radio.disabled = false;
-    btn_radio.style.color = 'var(--active-function-color)';
-    PlayWhiteNoise();
+function radio() {
+  btn_radio.disabled = true;
+  btn_pause_radio.disabled = false;
+  btn_radio.style.color = "var(--active-function-color)";
+  PlayWhiteNoise();
 }
 
-function pause(){
-    btn_radio.disabled = false;
-    btn_pause_radio.disabled = true;
-    btn_radio.style.color = 'var(--cor-primaria)';
+function pause() {
+  btn_radio.disabled = false;
+  btn_pause_radio.disabled = true;
+  btn_radio.style.color = "var(--cor-primaria)";
 
-    WhiteNoisePause();
+  WhiteNoisePause();
 }
 
 import StyleMode from "./modules/StyleMode.js";
-StyleMode()
+StyleMode();
 
-import {clickSound} from "./modules/SoundClick.js";
-import {PlayWhiteNoise} from "./modules/SoundClick.js";
-import {WhiteNoisePause} from "./modules/SoundClick.js";
+import { clickSound } from "./modules/SoundClick.js";
+import { PlayWhiteNoise } from "./modules/SoundClick.js";
+import { WhiteNoisePause } from "./modules/SoundClick.js";
