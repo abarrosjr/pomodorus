6f221502c44e28e464bf11582a52f995ae8edda6
Adriano Jr <adrianojunr@gmail.com>
2025-03-05 11:52:52 -0300

    Cria uma função principal para audios, melhora o código

--- src/js/modules/SoundClick.js
+++ src/js/modules/SoundClick.js
@@ -1,45 +1,43 @@
 let radio;
 
 export function clickSound() {
-  PlayClick();
+  playAudio("play", "const", "src/audio/clique_efeito.wav", false, 1);
 }
 
 export function PlayWhiteNoise() {
-  WhiteNoiseSound();
+  playAudio(
+    "play",
+    "no-const",
+    "src/audio/Deep-Sleep-Brown-Noise.mp3",
+    true,
+    1,
+  );
 }
 
 export function WhiteNoisePause() {
-  NoisePause();
+  playAudio("pause");
 }
 
-function PlayClick() {
-  playAudio("const", "src/audio/clique_efeito.wav", false, 1);
-}
-
-function WhiteNoiseSound() {
-  playAudio("no-const", "src/audio/Deep-Sleep-Brown-Noise.mp3", true, 1);
-}
-
-function playAudio(type, src, loop, volume) {
-  if (type == "const") {
-    const sound = (file) => {
-      return new Audio(file);
-    };
-    const radio = sound(src);
-    radio.volume = volume;
-    radio.loop = loop;
-    radio.play();
-  } else if (type == "no-const") {
-    const sound = (file) => {
-      return new Audio(file);
-    };
-    radio = sound(src);
-    radio.volume = volume;
-    radio.loop = loop;
-    radio.play();
+function playAudio(status, type, src, loop, volume) {
+  if (status == "play") {
+    if (type == "const") {
+      const sound = (file) => {
+        return new Audio(file);
+      };
+      const radio = sound(src);
+      radio.volume = volume;
+      radio.loop = loop;
+      radio.play();
+    } else if (type == "no-const") {
+      const sound = (file) => {
+        return new Audio(file);
+      };
+      radio = sound(src);
+      radio.volume = volume;
+      radio.loop = loop;
+      radio.play();
+    }
+  } else if (status == "pause") {
+    radio.pause();
   }
-}
-
-function NoisePause() {
-  radio.pause();
 }
